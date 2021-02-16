1fa36c5668ed05137f6732f33262986e3ce38cbc
ArtyomLavrishchev <temalggt540@gmail.com>
2021-02-17 00:22:05 +0300

    Updates

---
+++ precache-manifest.e22682acedbcce3f168a29898b26b040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc045f3a610c90d4783b366a42f263de",
+    "url": "/TODOLIST/index.html"
+  },
+  {
+    "revision": "5086c822fa52cdaa5751",
+    "url": "/TODOLIST/static/css/main.5d4402f2.chunk.css"
+  },
+  {
+    "revision": "f080a92216c7c9db2893",
+    "url": "/TODOLIST/static/js/2.cafb7c37.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TODOLIST/static/js/2.cafb7c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5086c822fa52cdaa5751",
+    "url": "/TODOLIST/static/js/main.934d3e60.chunk.js"
+  },
+  {
+    "revision": "ee336b57959fc40f89d4",
+    "url": "/TODOLIST/static/js/runtime-main.a25c4c00.js"
+  }
+]);
